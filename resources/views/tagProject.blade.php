6e3fa5e969c042dd658a187151e804976ed49441
= <=>
2019-05-15 14:22:04 +0100

    added fancy error image

--- resources/views/tagProject.blade.php
+++ resources/views/tagProject.blade.php
@@ -4,6 +4,7 @@
         <div class="col-12 col-md-7 p-4 offset-md-1">
             <h3 class="center-text">Showing Projects On {{$tag->name}}  </h3>
 
+            @if(count($projects) > 0)
             @foreach($projects as $project)
                 <h5> <a href="/projects/{{$project->id}}">{{$project->title}} </a></h5>
                     <span class="tag">{{$project->tag->name}}</span>
@@ -13,7 +14,13 @@
                 <hr>
             @endforeach
 
+            @else
+                <i class="fas fa-cloud-upload-alt fa-10x big-top-margin" style="margin-left: 20%"></i>
+                <h3>Ooops! Seems like no one has uploaded a project for {{$tag->name}}. Why not be the first</h3>
             {{--{{$projects->links()}}--}}
+                <a href="/projects/create" class="btn btn-lg btn-outline-primary" style="margin-left: 20%"><h4>Upload Projects </h4></a>
+                @endif
+
         </div>
 
         <!--- Tag Side Bar -->
